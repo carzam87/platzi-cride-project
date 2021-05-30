2ce3ddc3c81f7ba028c34c9b76826f9f1d873013
Carlos Zambrana <carlosz@nicasource.com>
2021-05-30 12:35:08 -0600

    first commit

---
+++ cride/users/apps.py
@@ -0,0 +1,10 @@
+"""Users app."""
+
+# django
+from django.apps import AppConfig
+
+
+class UserAppConfig(AppConfig):
+
+    name = 'cride.users'
+    verbose_name = 'Users'
---
+++ cride/users/models/users.py
@@ -0,0 +1,53 @@
+"""User model."""
+
+# Django
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+
+# utilities
+from cride.utils.models import CRideModel
+
+
+class User(CRideModel, AbstractUser):
+    """User model.
+
+    Extend from Django's Abstract User, change the username field
+     to email and add some extra fields
+    """
+
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_messages={
+            'unique': 'A user with that email already exists.'
+        }
+    )
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: +999999999"
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    is_client = models.BooleanField(
+        'client status',
+        default=True,
+        help_text=(
+            'Help easilly distinguish users and perform queries.'
+            'Clients are the main type of user.'
+        )
+    )
+    is_verify = models.BooleanField(
+        'verified',
+        default=False,
+        help_text='Set to true when the user have verified its email address.'
+    )
+
+    def __str__(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
