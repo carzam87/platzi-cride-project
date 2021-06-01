11ff59c6d4835b335b045e73f522139407b1172c
Carlos Zambrana <carlosz@nicasource.com>
2021-06-01 00:22:23 -0600

    WIP

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,9 +44,11 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
+    'rest_framework'
 ]
 LOCAL_APPS = [
-    'cride.users.apps.UserAppConfig'
+    'cride.users.apps.UserAppConfig',
+    'cride.circles.apps.CircleAppConfig'
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
---
+++ cride/circles/apps.py
@@ -0,0 +1,10 @@
+"""Circles app."""
+
+# django
+from django.apps import AppConfig
+
+
+class CircleAppConfig(AppConfig):
+
+    name = 'cride.circles'
+    verbose_name = 'Circles'
